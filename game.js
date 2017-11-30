55320ae8ee2110899b37170f681d2e963a2759af
Shannon Berry-Heller <shannonberry-heller@Shannons-MacBook.local>
2017-11-30 14:19:58 -0600

    prompt working

--- game.js
+++ game.js
@@ -1,47 +1,47 @@
 var inquirer = require("inquirer");
-var word = require("./word.js")
-var letter = require("./letter.js")
+//var word = require("./word.js")
+//var letter = require("./letter.js")
 
 /*--------Prompts user to begin game--------*/
 inquirer.prompt([{
 	name: "play",
 	type: "confirm",
 	message: "Want to play hangman?",
-	validate: function validateUserInput(play){
-		if(answer.play) {
-			//newGame();
+	//validate: function validateUserInput(play){
+	//	if(answer.play) {
+	//		//newGame();
+}]);
+//     console.log("==============================================");
+//     console.log("\n Guess a letter\n");
+//     console.log("==============================================");
+//   		}
+// 		else {
+// 			console.log("Then why are you here?")
+// 		}
+// 	}
 
-    console.log("==============================================");
-    console.log("\n Guess a letter\n");
-    console.log("==============================================");
-  		}
-		else {
-			console.log("Then why are you here?")
-		}
-	}
-
-}])
-//.then(startGame());
-
-
-//Function to start the game
-var startGame = function(){
-
-//vari
-	guesses = [];
-//Variable for user guess countdown
-	var guessesRemaining = [10];
-	inquirer.prompt([
-		{
-			type: "input"
-			message: "Guess a letter: ",
-			validate : function(input){
-			return /[a-z]/.test(input.trim().toLowerCase());
-		}
-	])
-};
+// }])
+// //.then(startGame());
 
 
-var resetGuessesRemaining = function(){
-	this.guessesRemaining = 10;
-},
+// //Function to start the game
+// var startGame = function(){
+
+// //vari
+// 	guesses = [];
+// //Variable for user guess countdown
+// 	var guessesRemaining = [10];
+// 	inquirer.prompt([
+// 		{
+// 			type: "input"
+// 			message: "Guess a letter: ",
+// 			validate : function(input){
+// 			return /[a-z]/.test(input.trim().toLowerCase());
+// 		}
+// 	])
+// };
+
+
+// var resetGuessesRemaining = function(){
+// 	this.guessesRemaining = 10;
+// },
